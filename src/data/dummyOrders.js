d0bbe95938a05fdd3b00bc46e59dc4f983591c95
jooeunhan <yeppijune@gmail.com>
2026-08-11 18:08:48 +0900

    :sparkles: Feat: 예약 주문 더미 데이터 가짓수 추가

--- src/data/dummyOrders.js
+++ src/data/dummyOrders.js
@@ -5,32 +5,102 @@ const dummyOrders = [
     orderNumber: "UPC-7K4D-92LM",
 
     productName: "Aren 비세토스 닥스훈트 참",
+
     store: "MCM 롯데면세점 명동본점",
 
     reservationStatus: "방문 예약",
-    reservationDate: "2026. 8. 18",
+
+    reservationDate: "2026. 08. 18",
+
     reservationTime: "14:00",
 
-    arrivalDate: "2026. 9. 24",
+    expectedArrivalDate: "2026. 09. 24",
+
+    barcodeValue: "UPC7K4D92LM20260818",
 
     steps: [
       {
         id: 1,
         label: "접수 완료",
         completed: true,
-        completedTime: "2026. 8. 18 14:32",
+        completedTime: "2026. 08. 18 14:32",
       },
       {
         id: 2,
         label: "본사 도착",
         completed: true,
-        completedTime: "2026. 8. 20 10:15",
+        completedTime: "2026. 08. 20 10:15",
       },
       {
         id: 3,
         label: "제품 검수",
         completed: true,
-        completedTime: "2026. 8. 21 16:40",
+        completedTime: "2026. 08. 21 16:40",
+      },
+      {
+        id: 4,
+        label: "제작 진행",
+        completed: false,
+        completedTime: null,
+      },
+      {
+        id: 5,
+        label: "제작 완료",
+        completed: false,
+        completedTime: null,
+      },
+      {
+        id: 6,
+        label: "배송 중",
+        completed: false,
+        completedTime: null,
+      },
+      {
+        id: 7,
+        label: "매장 도착",
+        completed: false,
+        completedTime: null,
+      },
+    ],
+  },
+
+  {
+    id: 2,
+
+    orderNumber: "UPC-8P2M-11AA",
+
+    productName: "MCM 비세토스 키링",
+
+    store: "MCM 롯데백화점 본점",
+
+    reservationStatus: "방문 예약",
+
+    reservationDate: "2026. 08. 25",
+
+    reservationTime: "16:30",
+
+    expectedArrivalDate: "2026. 10. 02",
+
+    barcodeValue: "UPC8P2M11AA20260825",
+
+    steps: [
+      {
+        id: 1,
+        label: "접수 완료",
+        completed: true,
+        completedTime: "2026. 08. 25 16:48",
+      },
+      {
+        id: 2,
+        label: "본사 도착",
+        completed: false,
+        completedTime: null,
+      },
+      {
+        id: 3,
+        label: "제품 검수",
+        completed: false,
+        completedTime: null,
       },
       {
         id: 4,
