aca2166ab3b056f9368c0bf27d6d2333206db283
jooeunhan <yeppijune@gmail.com>
2026-08-14 20:38:19 +0900

    :sparkles: Feat: 구조 재사용을 위한 props 추가

--- src/pages/MainPage/BrandIntro.jsx
+++ src/pages/MainPage/BrandIntro.jsx
@@ -50,17 +50,22 @@ const FooterText = styled.p`
   margin: 0;
 `;
 
-function BrandIntro() {
+function BrandIntro({ showIntro = true }) {
   return (
     <SectionWrapper>
-      <TitleText>MCM: 대담한 럭셔리 여행을 향한 헌사</TitleText>
-      <BodyText>
-        1976년부터 MCM은 장인정신과 혁신을 바탕으로 전통적인 럭셔리의 개념을
-        새롭게 정의해왔습니다. 젊음과 독립성, 그리고 쉽게 알아볼 수 있는
-        디자인 감각을 지닌 MCM은 프리미엄 백과 백팩, 가죽 액세서리, 의류
-        컬렉션을 통해 편안함과 품격을 동시에 추구하는 라이프스타일을
-        제안합니다.
-      </BodyText>
+      {showIntro && (
+        <>
+          <TitleText>MCM: 대담한 럭셔리 여행을 향한 헌사</TitleText>
+
+          <BodyText>
+            1976년부터 MCM은 장인정신과 혁신을 바탕으로 전통적인 럭셔리의 개념을
+            새롭게 정의해왔습니다. 젊음과 독립성, 그리고 쉽게 알아볼 수 있는
+            디자인 감각을 지닌 MCM은 프리미엄 백과 백팩, 가죽 액세서리, 의류
+            컬렉션을 통해 편안함과 품격을 동시에 추구하는 라이프스타일을
+            제안합니다.
+          </BodyText>
+        </>
+      )}
       <BadgeWrapper>
         <BadgeImage src={butterflyMarkBadge} alt="Butterfly Mark Certified" />
       </BadgeWrapper>
